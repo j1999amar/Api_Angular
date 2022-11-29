7de095331c8d1dc6c5440435a52938029ae5a49d
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-29 09:02:49 +0530

    First Commit

---
+++ src/app/passenger-data/passenger-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passenger-data',
+  templateUrl: './passenger-data.component.html',
+  styleUrls: ['./passenger-data.component.css']
+})
+export class PassengerDataComponent {
+
+}
